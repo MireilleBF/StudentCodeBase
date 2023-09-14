24ba7e03d6f8b18691e1a687a7c5a0360b505676
Mireille Blay-Fornarino <Mireille Blay Mireille.BLAY@univ-cotedazur.fr>
2023-09-14 12:25:19 +0200

    CODES TD2

---
+++ HARMO/HARMO_TD2/CODES/src/partII/ConstantForHarmo.java
@@ -0,0 +1,10 @@
+package v23_24.partII;
+
+public class ConstantForHarmo {
+    public static final int CURRENT_YEAR = 2023;
+    public static final int DEFAULT_BIRTH_YEAR = 1901;
+
+    private ConstantForHarmo() {
+    }
+
+}
